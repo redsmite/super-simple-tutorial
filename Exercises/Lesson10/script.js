2ed28a9540f63bfb1cba549390515568489498ca
Kym <denrncrkvcarabeo@gmail.com>
2024-08-28 00:18:54 +0800

    lesson 10 completed

---
+++ Exercises/Lesson10/script.js
@@ -0,0 +1,19 @@
+function testClassList(){
+  const button = document.querySelector('.js-button');
+    
+    if (button.classList.contains('js-button')) {
+        console.log("The button has the 'js-button' class.");
+    } else {
+        console.log("The button does not have the 'js-button' class.");
+    }
+}
+
+function clickToggle(className){
+  let button = document.querySelector(`.${className}`);
+
+  if (button && button.classList.contains('is-toggled')) {
+    button.classList.remove('is-toggled');
+  } else {
+    button.classList.add('is-toggled');
+  }
+}
